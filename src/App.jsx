0c766b387c9b1b0bb543cbd6ba7f308dbc53dfee
shafee-ahmed <officialshafee54@gmail.com>
2025-05-17 16:02:24 +0600

    Update .gitignore and project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom';
-import SignUp from "./components/SingUp"
-import Login from "./components/Login" 
-import Home from "./components/Home" 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from "./components/SingUp";
+import Login from "./components/Login";
+import Home from "./components/Home";
 import './App.css';
-function App() {
 
+function App() {
   return (
-    <Router>
-      <div >
-        <section>                              
-            <Routes>                                                                      
-               <Route path="/" element={<SignUp/>}/>
-               <Route path="/Login" element={<Login/>}/>
-               <Route path="/Home/*" element={<Home/>}/>
-            </Routes>                    
+    <BrowserRouter basename="/chat-app">
+      <div>
+        <section>
+          <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Home/*" element={<Home />} />
+          </Routes>
         </section>
       </div>
-    </Router>
- )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
